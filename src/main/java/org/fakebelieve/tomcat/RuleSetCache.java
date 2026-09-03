3bb8fcc14d9211e8e70dada78344853491e03647
Joseph Mocker <mock@fakebelieve.org>
2026-09-03 16:29:31 -0700

    started reorganizing code

---
+++ src/main/java/org/fakebelieve/tomcat/RuleSetCache.java
@@ -0,0 +1,12 @@
+package org.fakebelieve.tomcat;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RuleSetCache {
+
+    private static final ConcurrentHashMap<String, SharedRuleSet> RULE_CACHE = new ConcurrentHashMap<>();
+
+    public static SharedRuleSet get(String key) {
+        return RULE_CACHE.computeIfAbsent(key, k -> new SharedRuleSet());
+    }
+}
